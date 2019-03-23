e60791113d9b7edc7ca56f1bb1225a65bea52854
mac <mac@macbook-pro.local>
2019-03-23 11:20:06 -0300

    Initial Commit

---
+++ Coordinators/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Coordinators
+//
+//  Created by mac on 23/03/19.
+//  Copyright © 2019 Wesley S. Favarin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
